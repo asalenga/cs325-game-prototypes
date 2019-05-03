9f4016036a7326d95f98a7c319714860f5777bac
asalenga <47401439+asalenga@users.noreply.github.com>
2019-05-03 11:12:03 -0400

    Update main.js

--- DigitalAssignment2/js/main.js
+++ DigitalAssignment2/js/main.js
@@ -249,15 +249,15 @@ window.onload = function() {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, "I mean... I guess that's an A for effort.", { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("I mean... I guess that's an A for effort.");
         }
-        else if (score <= 1650) {
+        else if (score <= 2000) {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, 'Hey not bad, you could do better though.', { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText('Hey not bad, you could do better though.');
         }
-        else if (score <= 2000) {
+        else if (score <= 3000) {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, 'Noice! You beat my single player high score!', { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("Noice! You beat my single player high score!");
         }
-        else if (score <= 3000) {
+        else if (score <= 4000) {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, "Weow. That's a solid number right there. How many times\nhave you played this?\n\n...Try-hard.", { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("Weow. That's a solid number right there. How many times\nhave you played this?\n\n...Try-hard.");
         }
@@ -265,11 +265,11 @@ window.onload = function() {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, "Hey, you do realize that you still have a life to live, right?\nGo hang out with friends or something.\nStop playing this game.", { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("Hey, you do realize that you still have a life to live, right?\nGo hang out with friends or something.\nStop playing this game.");
         }
-        else if (score <= 5000) {
+        else if (score <= 6000) {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, "If you're seeing this message, well I don't believe you.\nI'm sitting here at 4 in the morning typing this out.\nWhy am I still awake? Idk. You tell me. You probably hacked the game\nor something, bc you weren't supposed to get " +score+" points.\nThanks for playing!\nI don't have any new messages beyond the 3000 point mark...\nor do I...? ;)", { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("If you're seeing this message, well I don't believe you.\nI'm sitting here at 4 in the morning typing this out.\nWhy am I still awake? Idk. You tell me. You probably hacked the game\nor something, bc you weren't supposed to get " +score+" points.\nThanks for playing!\nI don't have any new messages beyond the 3000 point mark...\nor do I...? ;)");
         }
-        else if (score > 6000) {
+        else if (score > 7000) {
             //game.add.text(game.world.centerX-250, game.world.centerY+150, "Honestly, what is life? Who are you? Are you even real? You've seriously beaten this game.\nWhat a legend. Here's your reward: a thumbs up. Now stop playing. Seriously.", { fontSize: '20px', fill: '#FFFFFF' });
             gameOverComment.setText("Honestly, what is life? Who are you? Are you even real? You've seriously beaten this game.\nWhat a legend. Here's your reward: a thumbs up. Now stop playing. Seriously.");
         }
