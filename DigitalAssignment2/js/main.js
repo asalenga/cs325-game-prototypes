daaadf52e8b1b08a2d0a51af563ffe6457055de2
asalenga <47401439+asalenga@users.noreply.github.com>
2019-05-03 10:16:48 -0400

    Update main.js

--- DigitalAssignment2/js/main.js
+++ DigitalAssignment2/js/main.js
@@ -183,7 +183,7 @@ window.onload = function() {
             {
                 laser.reset(player1.x + player1.body.width/2.0 - 7, player1.y - 60);
                 laser.body.velocity.y = -300;
-                laserTime1 = game.time.now + 350;
+                laserTime1 = game.time.now + 250;
             }
         }
 
@@ -199,7 +199,7 @@ window.onload = function() {
             {
                 laser.reset(player2.x + player2.body.width/2.0 - 7, player2.y - 50);
                 laser.body.velocity.y = -300;
-                laserTime2 = game.time.now + 350;
+                laserTime2 = game.time.now + 250;
             }
         }
 
